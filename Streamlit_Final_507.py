c75db5b0624cd15281094e835d86ba76125baf4f
Alejandro <85662079+Alherrera2798@users.noreply.github.com>
2021-12-22 17:27:29 -0500

    Add files via upload

---
+++ Streamlit_Final_507.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 20 01:09:14 2021
+
+@author: Alejandro Herrera
+"""
+#Import packages
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
